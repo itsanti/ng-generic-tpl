eee2caf27764e68f8c92233464f0e823f17ac155
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-05-13 12:35:09 +0300

    Создание простого компонента кнопки

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './shared/components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, ButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { TuiButton } from '@taiga-ui/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [TuiButton],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {
+  @Input()
+  appearance: 'primary' | 'secondary' = 'primary';
+
+  @Input()
+  size: 's' | 'm' | 'l' = 'm';
+
+  @Input()
+  disabled = false;
+}
