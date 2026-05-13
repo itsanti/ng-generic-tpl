72eb7e0375e078943cd3b6284efff92658fc2555
Aleksandr Kurov <kurov.aleksandr@gmail.com>
2026-05-13 13:42:09 +0300

    Поддержка элементов Web Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { TuiRoot } from '@taiga-ui/core';
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './shared/components/button/button.component';
 
@@ -8,6 +8,7 @@ import { ButtonComponent } from './shared/components/button/button.component';
   imports: [RouterOutlet, TuiRoot, ButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // для Web Components элементов
 })
 export class App {
   protected readonly title = signal('generic-ng-tpl');
